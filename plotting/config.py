5c5df528c73d26188f097acef01194c9d5080cd5
Toni Esteves <toni.esteves@gmail.com>
2025-04-06 13:11:33 -0300

    create plotting mudule

---
+++ plotting/config.py
@@ -0,0 +1,11 @@
+#  Toni Esteves Copyright (c) 2025.
+#  Project Name: finance-optimization-models
+#  Author: Toni Esteves <toni.esteves@gmail.com.br>
+#  Created: 06/04/2025 13:01
+#  Updated: 06/04/2025 13:01
+
+# Example: Custom style
+PLOT_STYLE = {
+    "figure.figsize": (10, 6),
+    "font.size": 12,
+}
